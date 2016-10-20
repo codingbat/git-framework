d2c6eadb5f47ef6103fa36e9c8de31cb7ee43f2c
Nazmul Alam <Nazmul.Alam@students.ittralee.ie>
2016-10-20 23:10:00 +0100

    added profile module

---
+++ git-profile/src/main/java/com/nazmul/git/profile/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Using strategy pattern to choose an avatar for a GitHub user.
+ *
+ * Intent
+ * a) Separate superclass from sublass
+ * b) Capture abstraction in an interface
+ * c) Define a family of algorithms, encapsulate each one, and make them interchangeable
+ *
+ *
+ */
+
+package com.nazmul.git.profile;
