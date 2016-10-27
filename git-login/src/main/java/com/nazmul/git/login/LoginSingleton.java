7c6063d8da3c0d496d0e3e7443d9d9f7dea8d7af
Nazmul Alam <t00152975@365s.ittralee.ie>
2016-10-27 14:54:06 +0100

    implementing my own REST calls to Git API

--- git-login/src/main/java/com/nazmul/git/login/LoginSingleton.java
+++ git-login/src/main/java/com/nazmul/git/login/LoginSingleton.java
@@ -1,8 +1,17 @@
 package com.nazmul.git.login;
 
+import org.apache.http.HttpResponse;
 import org.eclipse.egit.github.core.client.GitHubClient;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Serializable;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 /**
  * By  making the Singleton class as a final class, it allows the compiler to apply performance
@@ -64,5 +73,17 @@ public final class LoginSingleton extends GitHubClient implements Serializable {
   public boolean isSignedIn() {
 
     return signedIn;
+  }
+
+  public static void login(String login, String password) throws ClientProtocolException, IOException {
+    HttpClient client = new DefaultHttpClient();
+    HttpGet request = new HttpGet("https://api.github.com");
+    HttpResponse response = client.execute(request);
+    BufferedReader rd = new BufferedReader (
+            new InputStreamReader(response.getEntity().getContent()));
+    String line = "";
+    while ((line = rd.readLine()) != null) {
+      System.out.println(line);
+    }
   }
 }
